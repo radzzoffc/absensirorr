45409c8f16c56d1eeaee009552d44d652052c0c9
Radzz Offc <radzzoffc@gmail.com>
2025-01-17 16:31:54 +0700

    Update radzzoffc.js

--- radzzoffc.js
+++ radzzoffc.js
@@ -8,7 +8,7 @@ submitButton.addEventListener("click", () => {
     const enteredPassword = passwordInput.value;
 
     if (enteredPassword === correctPassword) {
-        window.location.href = "siliwangiteamdigi.html";
+        window.location.href = "siliwangiteamdigi2.html";
     } else {
         errorMessage.style.display = "block";
     }
