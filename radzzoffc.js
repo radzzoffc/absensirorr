8cd1cebcbe45ac6beba2f1af4acead7d428e26b0
Radzz Offc <radzzoffc@gmail.com>
2025-01-17 09:56:39 +0700

    Update radzzoffc.js

--- radzzoffc.js
+++ radzzoffc.js
@@ -1,20 +1,15 @@
-// Define the correct password
-const correctPassword = "12345";
+const correctPassword = "siliwangidigi";
 
-// Get references to the DOM elements
 const passwordInput = document.getElementById("password");
 const submitButton = document.getElementById("submit");
 const errorMessage = document.getElementById("error-message");
 
-// Add an event listener to the submit button
 submitButton.addEventListener("click", () => {
     const enteredPassword = passwordInput.value;
 
     if (enteredPassword === correctPassword) {
-        // Redirect to the protected page if the password is correct
         window.location.href = "absenmonitoring.html";
     } else {
-        // Show error message if the password is incorrect
         errorMessage.style.display = "block";
     }
 });
